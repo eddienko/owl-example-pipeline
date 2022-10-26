b71d82a2f1bf6636ad94f706afb26c6cc00b63b7
Eduardo Gonzalez <eglez@gilgalad.co.uk>
2022-10-26 18:11:42 +0100

    Add input and output paths

--- owl_example/schema.py
+++ owl_example/schema.py
@@ -3,7 +3,11 @@ from pathlib import Path
 import voluptuous as vo
 
 schema = vo.Schema(
-    {vo.Required("datalen"): vo.Range(1, 1000), vo.Optional("output"): vo.Coerce(Path)}
+    {
+        vo.Required("datalen"): vo.Range(1, 1000),  
+        vo.Optional("input_path"): vo.Coerce(Path), 
+        vo.Optional("output_path"): vo.Coerce(Path),
+    }
 )
 """
 Example pipeline configuration schema.
